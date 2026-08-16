4b2923d4a6d1b7d1a5effbecba3486a2c9fd0cee
PhanTrieuCuong123 <phantrieucuong095@gmail.com>
2026-08-16 12:21:22 +0700

    Xóa File bị đè

--- UDM08_CHAT_PYTHON-main/Code/server/server_logger.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from threading import Lock
-
-_log_lock = Lock()
-
-def log(message: str) -> None:
-    now = datetime.now().strftime("%H:%M:%S")
-    with _log_lock:
-        print(f"[{now}] {message}", flush=True)
